7df5e9275919f6a6fc136c092926a8c0a30268a1
Fancying77 <1063921603@qq.com>
2026-01-09 17:55:53 +0800

    Create leancloud.js

---
+++ src/leancloud.js
@@ -0,0 +1,10 @@
+import AV from 'leancloud-storage'
+
+// 初始化 LeanCloud
+AV.init({
+  appId: 'dKlfO5mUckWHquQVoVVzkMEn-MdYXbMMI',
+  appKey: 'DsHFs5lSFzPL9Q2lwH2BPTWh',
+  serverURL: 'https://dklfo5mu.api.lncldglobal.com'
+})
+
+export default AV
